78abc1138a29ea5e38612595691dcf2e1e745667
BernH4 <bernhard.steidl14mail.com>
2020-06-11 18:32:26 +0200

    +- operator added, basic functionality is given

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll('button');
-let test;
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => updateDisplay(e));
@@ -12,24 +11,33 @@ function updateDisplay(e) {
     //Check if pressed button is a Valid Number or "."
     if (/[0-9.]/.test(operator)) display.textContent += operator;
     else if (operator == "÷" || operator == "×" || operator == "-" || operator == "+") {
+        //If an operator is pressed before any number input a "0" is put before the operator to make math possible
         if (display.textContent == "") display.textContent = `0 ${operator} `;
         else display.textContent += ` ${operator} `;
     }
     else if (operator == "C") display.textContent = "";
-    else if (operator == "DEL") display.textContent = display.textContent.slice(-2,-1);
-    //else if (operator == "=") calculate();
-    
-    opArr = display.textContent.match(/[÷×\-+]/g) || [];
-    console.log(opArr);
-    if (opArr.length == 2 || operator == "=") {
-        const numbers = display.textContent.match(/[0-9]*\.?[0-9]+/g);
-        console.log(numbers);
-        const lastChar = display.textContent.slice(-2,-1);
-        operate(opArr[0], numbers[0], numbers[1], lastChar); 
-        //console.log(opArr);
+    else if (operator == "DEL") {
+        //If the last character was a operator remove him and the 2 outter spaces, else just the number
+        if (display.textContent.slice(-1) == " ") display.textContent = display.textContent.slice(0,-3);
+        else display.textContent = display.textContent.slice(0,-1);
     }
-    //console.log(opArr);
-
+    //This function was made last, if i would programm everything again i would put the string immidiately
+    //into an array, i didnt do that so ill make a arr right now
+    else if (operator == "+-") {
+        const arr = display.textContent.split(" ")
+        lastElement = arr.length - 1;
+        //if the last element includes - remove it, else add it
+        if (arr[lastElement].includes("-")) arr[lastElement] = arr[lastElement].slice(1);
+        else arr[lastElement] = `-${arr[lastElement]}`;
+        display.textContent = arr.join(" ");
+    }
+    //Transforms the display string into an array and puts the numbers into the operate function    
+    contentArr = display.textContent.split(" ");
+    console.log(contentArr);
+    //length must be 5 not 4 because a space is also included as last array item (-> console.log)
+    if (contentArr.length == 5 || operator == "=") {
+        operate(contentArr[1],contentArr[0],contentArr[2],contentArr[3]);
+    }
 }
 
 
@@ -47,26 +55,15 @@ function operate(operator, a, b, lastChar) {
           result = a - b;
           break;
         case "+":
+          //the + before a and b is to transform the string into a number else it would just concat
+          // example 5 + 5: just a + a  would concat a b, result would be 55
           result = +a + +b;
           break;
         default:
-          console.log("ERROR")
+          display.textContent = "ERROR";
           break;
     }
     display.textContent = result;
-    console.log(lastChar);
+    //if the operate function was not triggered by "=" but by a operator concat that operator after the result
     if (/[÷×\-+]/.test(lastChar)) display.textContent += ` ${lastChar} `
 }
-
-//function calculate() {
-//    const textArr = display.textContent.split("");
-//    const solution = 9999;
-//    console.log(textArr);
-//    for(let i = 0; i < textArr.length; i++) {
-//        if (textArr[i] == "÷" || textArr[i] == "×") {
-//            //Bug: not working with 2 digits
-//            console.log(textArr[i], textArr[i-2], textArr[i+2]);
-//            //operate(textArr[i], textArr[i-2], textArr[i+2]);
-//        }
-//    }
-//}
